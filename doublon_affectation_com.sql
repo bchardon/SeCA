df6ac8008dffbb2a0b126d6f1b92f589d48e0930
bchardon <bastien.chardonnens@gmail.com>
2016-03-11 15:41:35 +0100

    correction bug

--- doublon_affectation_com.sql
+++
@@ -1,6 +0,0 @@
-﻿
-SELECT * FROM (SELECT NUFECO, shape_area, shape_leng, COUNT(*) as FREQ
-FROM oca1032s_affectation_com 
-group by NUFECO, shape_area, shape_leng 
-order by FREQ desc) AS DOUBLON
-WHERE FREQ > 1
