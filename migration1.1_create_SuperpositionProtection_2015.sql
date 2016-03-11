cf4464d8dcc015edb959b050983416abaed081ef
bchardon <bastien.chardonnens@gmail.com>
2016-03-11 15:15:27 +0100

    Ajout de l'étape création superpositionprotection

---
+++ migration1.1_create_SuperpositionProtection_2015.sql
@@ -0,0 +1,59 @@
+﻿/*CREATION DU NOUVEAU LAYER SuperpositionProtection
+*/
+
+
+	TRUNCATE superpositionprotection;
+	
+	-- DELETE les ROW ou TYPSTD = ZP* et TYPPROT = 'PAY' ET LES PLACE DANS SECTEUR SUPERPOSE.
+
+	INSERT INTO superpositionprotection (geom,nufeco,nosect,lieu,publieedep,statutjuri,shape_leng,shape_area,remarques,operateur,typsupprot)
+	SELECT aff.geom,
+	    aff.nufeco,
+	    aff.nosect,
+	    aff.lieu,
+	    aff.publieedepuis,
+	    aff.statutjuridique,
+	    aff.shape_leng,
+	    aff.shape_area,
+	    aff.remarq,
+	    aff.operat,
+	    'ZPP'
+	FROM oca1032s_affectation_2015 aff
+	WHERE typstd LIKE 'ZP%'
+	AND TYPPROT = 'PAY';
+
+	DELETE FROM oca1032s_affectation_2015 
+	WHERE typstd LIKE 'ZP%'
+	AND TYPPROT = 'PAY';
+
+	-- DELETE les ROW ou TYPSTD = ZP* et TYPPROT = 'NAT' ET LES PLACE DANS SECTEUR SUPERPOSE.
+	
+	INSERT INTO superpositionprotection (geom,nufeco,nosect,lieu,publieedep,statutjuri,shape_leng,shape_area,remarques,operateur,typsupprot)
+	SELECT aff.geom,
+	    aff.nufeco,
+	    aff.nosect,
+	    aff.lieu,
+	    aff.publieedepuis,
+	    aff.statutjuridique,
+	    aff.shape_leng,
+	    aff.shape_area,
+	    aff.remarq,
+	    aff.operat,
+	    'ZPN'
+	FROM oca1032s_affectation_2015 aff
+	WHERE typstd LIKE 'ZP%'
+	AND TYPPROT = 'NAT';
+
+	DELETE FROM oca1032s_affectation_2015 
+	WHERE typstd LIKE 'ZP%'
+	AND TYPPROT = 'NAT';
+	
+	/* OPTION: AJOUTER UN ATTRIBUT ZONEouSUPERPOS AFIN 
+	DE POUVOIR DETERMINER l'APPARTENANCE DE CHAQUE POLYGON A 
+	UNE ZONE OU UN CONTENU SUPERPOSE */
+	
+	ALTER TABLE superpositionprotection 
+	ADD COLUMN ZONEouSUPERPOS character varying(6);
+
+	
+	
