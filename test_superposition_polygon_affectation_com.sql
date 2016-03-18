e023fdc44671299590ebe698fefc5b03e6681c4a
bchardon <bastien.chardonnens@gmail.com>
2016-03-18 11:17:37 +0100

    Modification/ajout

--- test_superposition_polygon_affectation_com.sql
+++ test_superposition_polygon_affectation_com.sql
@@ -1,6 +1,6 @@
 ﻿-- Necessite environ 8-10 minutes pour tourner.
 
-SELECT ST_AREA(ST_UNION(g1geom,g2geom)) as AREA, g1gid, g2gid
+SELECT ST_AREA(ST_INTERSECTION(g1geom,g2geom)) as AREA, g1gid, g2gid
 FROM 
 (SELECT g1.geom as g1geom, g2.geom as g2geom, g1.gid as g1gid, g2.gid as g2gid
 FROM oca1032s_affectation_2015 g1, oca1032s_affectation_2015 g2
